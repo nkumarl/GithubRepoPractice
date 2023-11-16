9ca580c403708d3a1434b947a85ee41b03f4a533
nirdevops <idsnirmal@gmail.com>
2023-11-17 00:10:51 +0530

    add src/app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,27 @@
-print ("app, py file"ac)
+print ("hello, world file")
+
+# Simple Python code for addition
+
+# Function to add two numbers
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# Example usage
+number1 = 5
+number2 = 7
+
+# Calling the function and printing the result
+sum_result = add_numbers(number1, number2)
+print(f"The sum of {number1} and {number2} is: {sum_result}")
+
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+# Example usage
+num1 = 10
+num2 = 5
+result = subtract(num1, num2)
+print(f"The result of {num1} - {num2} is: {result}")
